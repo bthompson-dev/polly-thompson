221f5c47e2e1ad886b3a8acfd549080a4a78ef32
Ben Thompson <ben_thompson12@hotmail.com>
2024-07-23 15:22:49 +0100

    added about section and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <a href='https://www.linkedin.com/in/polly-thompson-84096779/'>
+            <img src="src/img/svg/linkedin-reposition2.svg" alt="LinkedIn" />
+        </a>
+    </footer>
+  )
+}
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import '../css/home.css'
-import NavBar from '../components/NavBar'
+import React from "react";
+import "../css/home.css";
+import NavBar from "../components/NavBar";
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
-    <div className='background background-one'></div> 
+      <div className="background background-one"></div>
       <NavBar />
-    <div className="background background-two"></div>
-  
+      <div className="background background-two"></div>
+      <div className="about">
+        <div className="about__content">
+          <h2>About</h2>
+          <img src='src/img/polly-profile.jpg' alt="Polly Thompson Profile" />
+          <div className="about__content--text">
+            A multi-lingual journalist with an MA in International Journalism
+            from City, University of London. I have experience reporting abroad
+            in Argentina and the US, and reported on Latin America, culture,
+            tech, politics, and space. I'm passionate about broadcast journalism
+            and finding creative ways to share important stories and make
+            complex topics approachable for viewers and listeners. I was just
+            nominated by my course head for the Foreign Press Association's
+            'Future Foreign Correspondent (student) of the Year' award for a
+            video report on Louisiana's death row.
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Home;
